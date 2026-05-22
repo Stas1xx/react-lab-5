4c38694ac8771bdb8573a4c39dccd7dc22779487
Stas1xx <s.ivanchenko07@gmail.com>
2026-05-22 18:26:22 +0200

    foto

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 
 export default function ImageGallery() {
   const images = [
-    "https://dummyimage.com/200x200/000/fff&text=Фото+1",
-    "https://dummyimage.com/200x200/f00/fff&text=Фото+2",
-    "https://dummyimage.com/200x200/00f/fff&text=Фото+3"
+    "https://i1-e.pinimg.com/1200x/3f/18/99/3f189916c374ec0768e8db122d203ce5.jpg",
+    "https://i1-e.pinimg.com/736x/9b/e4/ed/9be4ed6b420b83c105b1ee3a39586ed4.jpg",
+    "https://i1-e.pinimg.com/736x/78/d7/67/78d767059701c1b9fe341d23f17eb8c2.jpg"
   ];
   const [index, setIndex] = useState(0);
 
